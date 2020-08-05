77fa74c506bc3d69660fe6556d76d389b178b25d
lpercivalDEV <lpercival.dev@gmail.com>
2020-08-04 23:15:08 -0400

    First commit - project setup

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary:  '#FFFFFF',
+        secondary: '#191919',
+      },
+      fontFamily: {
+        sans: ['Montserrat'],
+        serif: ['Spectral'],
+        mono: ['Montserrat'],
+        display: ['Montserrat'],
+        body: ['Spectral'],
+      }
+
+    },
+  },
+  variants: {},
+  plugins: [],
+}
