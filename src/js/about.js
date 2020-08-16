e66abe1bf3a08952b6acbb898fca16514a4c21b9
unknown <lavellthedev+github@gmail.com>
2020-08-16 18:40:13 -0400

    pushing changes

--- src/js/about.js
+++ src/js/about.js
@@ -14,3 +14,24 @@ gsap.to(".panel:not(:last-child)", {
 });
 
 gsap.set(".panel", {zIndex: (i, target, targets) => targets.length - i});
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("employeeImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
