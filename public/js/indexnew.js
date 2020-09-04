7a89edd1930bab7c263b07fe84a15542ca3ba5e2
unknown <lavellthedev+github@gmail.com>
2020-09-04 04:39:53 -0400

    home page 90 percent finished

--- public/js/indexnew.js
+++ public/js/indexnew.js
@@ -14,24 +14,3 @@ gsap.to(".panel:not(:last-child)", {
 });
 
 gsap.set(".panel", {zIndex: (i, target, targets) => targets.length - i});
-
-
-//card carousel code
-let cardSelect = document.querySelectorAll(".card");
-
-for(let i=0; i<cardSelect.length; i++) {
-  cardSelect[i].addEventListener("click", pickCard);
-}
-
-function checkClass() {
-  for(let i=0; i<cardSelect.length; i++) {
-    cardSelect[i].classList.remove("active");
-    cardSelect[i].classList.add("card");
-  }
-}
-
-
-function pickCard(event) {
-  checkClass();
-  this.classList = "active";
-}
