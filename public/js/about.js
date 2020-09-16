2b7431d99dbe6dc036cff8aed209b328f07ccd2d
unknown <lavellthedev+github@gmail.com>
2020-09-16 00:11:35 -0400

    pushing changes you don't get sucks

--- public/js/about.js
+++ public/js/about.js
@@ -1,19 +1,4 @@
-// gsap.registerPlugin(ScrollTrigger);
-//
-// gsap.to(".panel:not(:last-child)", {
-//   yPercent: -100,
-//   ease: Linear.easeNone,
-//   stagger: 0.5,
-//   scrollTrigger: {
-//     trigger: "#container",
-//     start: "top top",
-//     end: "+=700%",
-//     scrub: 2,
-//     pin: true
-//   }
-// });
-//
-// gsap.set(".panel", {zIndex: (i, target, targets) => targets.length - i});
+
 
 // create references to the modal...
 let modal = document.getElementById('myModal');
